b70b69f547b733523edd46bb75794afad991cf50
FandiSetyoso <115622813+FandiSetyoso@users.noreply.github.com>
2022-11-20 20:36:06 +0700

    Add files via upload

---
+++ MODUL3 FANDI/MyItem.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <title>Document</title>
+    <link rel="stylesheet" href="\MODUL3 FANDI\style.css">
+</head>
+<body>
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav gap-3">
+                    <a href="" class="nav-link" style="color: white;">Home</a>
+                    <a href="" class="nav-link">Mycar</a>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+
+    <!-- Content -->
+    <section id="list">
+        <div class="container">
+        <div>
+            <h1>My Show Room</h1>
+            <p>List Show Room Fandi - 1202204103</p>
+            <div class="d-flex gap-5">
+            </div>
+        </div>
+        </div>
+    </section>
+    
+</body>
+</html>
